cf1c6af8c41bf8dc93ea292523723ba592cc4e01
Administrator <admin@example.com>
2019-08-21 20:36:05 +0300

    Change zone

--- variables.tf
+++ variables.tf
@@ -6,11 +6,11 @@ variable "project_name" {
 }
 
 variable "gloud_creds_file" {
-  default = "account.json"
+  default = "/root/.json/absolute-router-242207-e0142bb87119.json"
 }
 
 variable "location" {
-  default = "europe-west3"
+  default = "europe-west1"
 }
 
 variable "node-count"{
@@ -21,27 +21,7 @@ variable "machine_type" {
   default = "n1-standard-1"
 }
 
-// Database configuration
-variable "database_instance_name" {
-  default = "main-postgres-db"
-}
-
-variable "database_prod_user_pass" {
-  default = "x"
-}
-
-variable "database_test_user_pass" {
-  default = "x"
-}
-
 variable "kubernetes_ver" {
   default = "1.11"
 }
 
-# resource "random_id" "username" {
-#   byte_length = 14
-# }
-
-# resource "random_id" "password" {
-#   byte_length = 16
-# }
